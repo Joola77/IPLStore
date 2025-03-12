0798511077a16007a01b2ad2a27b73083ddb00f6
Joola77 <vasavi.joola2010@gmail.com>
2025-03-12 22:33:04 +0530

    Missing Functionality.
    Adding Back Button On all pages
    Added Missing Remove from Cart Functionality.

--- IPLStoreAPI/IPLStoreAPI/obj/Debug/net8.0/IPLStoreAPI.AssemblyInfo.cs
+++ IPLStoreAPI/IPLStoreAPI/obj/Debug/net8.0/IPLStoreAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPLStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407460748e543261e658d4b4cc0506f520319308")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPLStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPLStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
